0c934a63c3b95f4575aee6adeb6c37e26c8f0e2a
wilsonta <wilsonta@rose-hulman.edu>
2018-11-26 09:19:26 -0500

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -87,7 +87,7 @@ print(3607*34227)
 
 ###############################################################################
 #
-# : 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
